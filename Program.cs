05986ac65085362bcb51e56157f3d4cd276f944e
huseynmammadov1905 <115203208+huseynmammadov1905@users.noreply.github.com>
2023-08-30 14:22:58 +0400

    commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using HTTP;
+using System.Text.Json;
+//////Post
+//HttpClient client = new();
+
+//Post post = new()
+//{
+//    UserId = 1,
+//    Id = 1,
+//    Title = "Hello",
+//    Body = "World",
+//};
+
+//Uri uri = new Uri(@"https://jsonplaceholder.typicode.com/posts");
+
+//string json = JsonSerializer.Serialize(post);
+
+//StringContent stringContent = new(json);
+
+//HttpResponseMessage httpResponse = await client.PostAsync(uri, stringContent);
+
+//Console.WriteLine((int)httpResponse.StatusCode);
+
+//var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+
+//Console.WriteLine(jsonResponse);
+
+//////Put
+
+//HttpClient client = new();
+
+//Post post = new()
+//{
+//    UserId = 1,
+//    Id = 1,
+//    Title = "Hello",
+//    Body = "World",
+//};
+
+//Uri uri = new Uri(@"https://jsonplaceholder.typicode.com/posts/1");
+
+//string json = JsonSerializer.Serialize(post);
+
+//StringContent stringContent = new(json);
+
+//HttpResponseMessage httpResponse = await client.PutAsync(uri, stringContent);
+
+//Console.WriteLine((int)httpResponse.StatusCode);
+
+//var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+
+//Console.WriteLine(jsonResponse);
+
+
+
+//////Delete
+///
+
+
+
+HttpClient client = new();
+
+Post post = new()
+{
+    UserId = 1,
+    Id = 1,
+    Title = "Hello",
+    Body = "World",
+};
+
+Uri uri = new Uri(@"https://jsonplaceholder.typicode.com/posts/1");
+
+string json = JsonSerializer.Serialize(post);
+
+StringContent stringContent = new(json);
+
+HttpResponseMessage httpResponse = await client.DeleteAsync(uri);
+
+Console.WriteLine((int)httpResponse.StatusCode);
+
+var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+
+Console.WriteLine(jsonResponse);
